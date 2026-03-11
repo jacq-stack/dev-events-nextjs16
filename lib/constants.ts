1a4e1c162b9e16097796e5d6d69a1addf5f0b10a
jacq-stack <josearmando.jacq1@icloud.com>
2026-03-11 16:12:31 +0100

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+    {
+        image: "/images/event1.png",
+        title: "JSWorld Conference 2026",
+        slug: "jsworld-conference-2026",
+        location: "Amsterdam, Netherlands",
+        date: "2026-05-15",
+        time: "09:00 AM"
+    },
+    {
+        image: "/images/event2.png",
+        title: "EthGlobal London Hackathon",
+        slug: "ethglobal-london-2026",
+        location: "London, UK",
+        date: "2026-06-12",
+        time: "10:00 AM"
+    },
+    {
+        image: "/images/event3.png",
+        title: "React Summit Berlin",
+        slug: "react-summit-berlin",
+        location: "Berlin, Germany",
+        date: "2026-07-20",
+        time: "08:30 AM"
+    },
+    {
+        image: "/images/event4.png",
+        title: "DevOps Days New York",
+        slug: "devops-days-ny",
+        location: "New York, USA",
+        date: "2026-08-10",
+        time: "09:00 AM"
+    },
+    {
+        image: "/images/event5.png",
+        title: "Google I/O 2026",
+        slug: "google-io-2026",
+        location: "Mountain View, USA",
+        date: "2026-05-18",
+        time: "10:00 AM"
+    },
+    {
+        image: "/images/event6.png",
+        title: "VueConf US 2026",
+        slug: "vueconf-us-2026",
+        location: "Austin, USA",
+        date: "2026-09-05",
+        time: "09:00 AM"
+    }
+];
